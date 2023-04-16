2280ef8c86abe6eda2194a8e0d986f9bbfd64f27
sohyun <shlim0410@gmail.com>
2023-04-16 21:13:43 +0100

    handle scrolling smoothly

--- main.js
+++ main.js
@@ -1,13 +1,55 @@
-var i = 0;
-var txt = "I'm Sohyun, a Software Engineer!";
-var speed = 100;
+"use strict";
 
+/* handle typing */
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+  let i = 0;
+  let txt = "I'm Sohyun, a Software Engineer!";
+  return function type() {
+    if (i < txt.length) {
+      document.getElementById("typing").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(type, 100);
+    }
+  };
 }
 
-typeWriter();
+let type = typeWriter();
+type();
+
+/* handle scrolling*/
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
+
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+const contactBtn = document.querySelector(".about__contact");
+contactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+/* show arrow button when scrolling down */
+const about = document.querySelector("#about");
+const aboutHeight = about.getBoundingClientRect().height;
+const arrowBtn = document.querySelector(".arrow-btn");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > aboutHeight / 2) {
+    arrowBtn.classList.add("visible");
+  } else {
+    arrowBtn.classList.remove("visible");
+  }
+});
+
+/* scrolling when click arrow button */
+arrowBtn.addEventListener("click", () => {
+  scrollIntoView("#about");
+});
