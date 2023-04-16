47d5f852fa1a35c23dc509b69cd3405ba453ed5a
sohyun <shlim0410@gmail.com>
2023-04-16 18:25:30 +0100

    make responsible

---
+++ main.js
@@ -0,0 +1,13 @@
+var i = 0;
+var txt = "I'm Sohyun, a Software Engineer!";
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+typeWriter();
